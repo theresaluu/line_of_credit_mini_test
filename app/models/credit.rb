a66930160694297da94a7e7364a3e9da509d172d
Theresa Luu <theresa.luu@gmail.com>
2016-01-17 16:04:07 -0800

    Numericality Validations #min/max expectations
    Added range validations for numerical inputs

--- app/models/credit.rb
+++ app/models/credit.rb
@@ -1,4 +1,4 @@
 class Credit < ActiveRecord::Base
-  validates :apr, presence: true
-  validates :line_max, presence: true
+  validates :apr, presence: true, numericality: {less_than: 1.0, greater_than: 0.0}
+  validates :line_max, presence: true, numericality: {greater_than:0.0}
 end
--- test/models/credit_test.rb
+++ test/models/credit_test.rb
@@ -2,5 +2,7 @@ require 'test_helper'
 
 class CreditTest < ActiveSupport::TestCase
   should validate_presence_of(:apr)
+  should validate_numericality_of(:apr).is_less_than(1.0).is_greater_than(0.0)
   should validate_presence_of(:line_max)
+  should validate_numericality_of(:line_max).is_greater_than(0.0)
 end
