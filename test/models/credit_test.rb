feea348da2c10c783b37ede4e22bbe34766fd530
Theresa Luu <theresa.luu@gmail.com>
2016-01-17 16:12:44 -0800

    Validation Refactor #Array for presence required
    This might be overkill, but will be helpful esp if this grows to need
    more required fields.

--- test/models/credit_test.rb
+++ test/models/credit_test.rb
@@ -1,8 +1,9 @@
 require 'test_helper'
 
 class CreditTest < ActiveSupport::TestCase
-  should validate_presence_of(:apr)
+  [:apr, :line_max].each do |param|
+    should validate_presence_of(param)
+  end
   should validate_numericality_of(:apr).is_less_than(1.0).is_greater_than(0.0)
-  should validate_presence_of(:line_max)
   should validate_numericality_of(:line_max).is_greater_than(0.0)
 end
